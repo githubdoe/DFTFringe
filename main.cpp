18dcc69c094df036f0b1b4f9b5e6c4354d608b52
Julien Staub <atsju2@yahoo.fr>
2023-08-17 17:53:04 +0200

    add sdplog 1.12.0 sources to repo

--- main.cpp
+++ main.cpp
@@ -21,12 +21,31 @@
 #include "messagereceiver.h"
 #include "utils.h"
 #include <QDebug>
-
+#include "spdlog/spdlog.h"
+#include "spdlog/cfg/env.h"
+#include "spdlog/sinks/rotating_file_sink.h"
 
 
 int main(int argc, char *argv[])
-{    // Allow secondary instances
+{   // In order to set the log level to "info" and mylogger to "trace":
+    // SPDLOG_LEVEL=info,mylogger=trace && ./example
+    spdlog::cfg::load_env_levels();
+    spdlog::info("Welcome to spdlog version {}.{}.{}  !", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
 
+    // Create a file rotating logger with 5mb size max and 3 rotated files.
+    auto rotating_logger = spdlog::rotating_logger_mt("some_logger_name", "logs/rotating.txt", 1048576 * 5, 3);
+    // periodically flush all *registered* loggers every 10 seconds:
+    // warning: only use if all your loggers are thread-safe ("_mt" loggers)
+    spdlog::flush_every(std::chrono::seconds(10));
+
+    spdlog::trace("trace");
+    spdlog::debug("debug");
+    spdlog::info("info");
+    spdlog::warn("warn");
+    spdlog::error("error");
+    spdlog::critical("critical");
+
+    // Allow secondary instances
     SingleApplication app( argc, argv, true );
 
     MessageReceiver msgReceiver;
