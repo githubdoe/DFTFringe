bf6a323e507d6ba2bd2e9d072f024f89bd2a76c0
Julien Staub <atsju2@yahoo.fr>
2025-08-17 16:51:21 +0200

    fix typo

--- zernike/zapm_interface.h
+++ zernike/zapm_interface.h
@@ -1,6 +1,4 @@
 #include "armadillo"
-#include <QString>
-#include <Qvector>
 
 arma::mat zapm(const arma::vec &rho, const arma::vec &theta, const double &eps,
                const int &maxorder = 12);
